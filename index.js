c38cf64b96a39a0c7d4a4de8609b5e50bb7a9e50
Abu Naim <abunaim140@gmail.com>
2024-08-15 18:34:59 +0600

    implement get product count api

--- index.js
+++ index.js
@@ -34,6 +34,11 @@ async function run() {
             res.send(products);
         });
 
+        app.get('/product-count', async (req, res) => {
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({count});
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
